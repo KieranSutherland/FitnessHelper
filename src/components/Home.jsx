84b42ab993bde715f40063d9c3f599a0eb9cb760
Kieran Sutherland <35941396+KieranSutherland@users.noreply.github.com>
2018-06-24 01:11:17 +0100

    Added carousel to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Carousel } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { firebase } from '../firebase';
 import './css/Home.css';
 
 export default class Home extends Component {
   constructor(){
     super();
     this.state = {
-
+      carouselPosition: 'absolute',
+      carouselTop: '-9999px'
     }
+    }
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+          if(user) {
+            //If carousel won't fit on screen, remove it
+            if(window.innerWidth >= 970) {
+              this.setState({carouselPosition: '', carouselTop: ''})
+            }
+          }
+      })
     }
 
 
@@ -22,6 +36,27 @@ render() {
           FitnessHelper makes it easy to find the correct diet and exercise plan for your fitness goal.
         </p>
       </Jumbotron>
+
+      <div className='content-container'>
+
+        <Carousel className='carousel' style={{position: this.state.carouselPosition, top: this.state.carouselTop}}>
+          <Carousel.Item>
+            <NavLink to='/diet'><img alt="Diet" src={ require('./pics/diet.jpeg') }/></NavLink>
+            <Carousel.Caption>
+              <h3>Diet</h3>
+              <p>Get your diet in shape before you get yourself in shape.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <NavLink to='/exercise'><img alt="Exercise" src={ require('./pics/exercise.jpeg') }/></NavLink>
+            <Carousel.Caption>
+              <h3>Exercise</h3>
+              <p>Make progress. Not excuses.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+
+     </div>
 
     </main>
 
